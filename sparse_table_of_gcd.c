8c34d67fbd94107f99e9da89ca9b7fa02ba0f6d3
Yerzhan15 <mademihanov.erzhan@gmail.com>
2015-10-14 11:49:21 +0600

    Update sparse_table_of_gcd.c

--- sparse_table_of_gcd.c
+++ sparse_table_of_gcd.c
@@ -1,113 +1,104 @@
 #include <stdio.h>
-#include <math.h>
 
 
 #define N 500000
 
-int n, i, K, j;
-long long a[N];
-long long d[20][N];
+long long d[20][N + 1];
+int t[N + 1];
+int i, j, test, n, K;
 long long ans;
-int t[N];
-int test;
 
-long long max(long long a, long long b) {
-    if (a > b)
-        return a;
-    return b;
-}
-
-long long gcd(long long a, long long b) {
-    while (a * b) {
-        if (a < b) {
-            long long d = a;
-            a = b;
-            b = d;
-        }
-        a = a % b;
-    }
-    return a + b;
-}
-
-long long GCD(int l, int r) {
-    int dif = t[r - l + 1];
-//  printf("%d %d dif is : %d ", l, r, dif);
-    return gcd(d[dif][l], d[dif][r - (1 << dif) + 1]);
-}
-
-int lower(int l, int r, int R, long long cur) {
-    int ans;
-    while (r - l >= 0) {
-        int m = (l + r) / 2;
-        if (GCD(m, R) == cur) {
-            r = m - 1;
-            ans = m;
-        } else
-            l = m + 1;
-    }
-    return ans;
-}
-
-int upper(int l, int r, int L, long long cur) {
-    int ans;
-    while (r - l >= 0) {
-        int m = (l + r) / 2;
-        if (GCD(L, m) == cur) {
-            l = m + 1;
-            ans = m;
-        } else
-            r = m - 1;
-    }
-    return ans;
-}
-
-int two(int i) {
+int is(int i) {
     int m = 0;
     while ((1 << m) < i)
         m++;
     return 1 * ((1 << m) - i == 0);
 }
 
+long long max(long long a, long long b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+long long gcd(long long a, long long b) {
+	while (a * b) {
+		if (a < b) {
+			long long d = a;
+			a = b;
+			b = d;
+		}
+		a = a % b;
+	}
+	return a + b;
+}
+
+long long GCD(int l, int r) {
+	int k = t[r - l + 1];
+	return gcd(d[k][l], d[k][r - (1 << k) + 1]);
+}
+
+int lower(int l, int r, int R, long long cur) {
+	int res;
+	while (r - l >= 0) {
+		int m = (l + r) / 2;
+		if (GCD(m, R) == cur) {
+			res = m;
+			r = m - 1;
+		} else {
+			l = m + 1;
+		}
+	}
+	return res;
+}
+
+int upper(int l, int r, int L, long long cur) {
+	int res;
+	while (r - l >= 0) {
+		int m = (l + r) / 2;
+		if (GCD(L, m) == cur) {
+			res = m;
+			l = m + 1;
+		} else {
+			r = m - 1;
+		}
+	}
+	return res;
+}
+
 int main(void) {
-    // your code goes here
-    
-    scanf("%I64d", &test);
-    
-    for (i = 2; i <= N; i++) {
-        t[i] = t[i - 1] + two(i);
-    }
-    
-    while (test--) {
-        scanf("%I64d", &n);
-        ans = 0;
-        for (i = 1; i <= n; i++) {
-            scanf("%I64d", &a[i]);
-            ans = max(ans, a[i]);
-        }
-        
-        for (i = 1; i <= n; i++)
-            d[0][i] = a[i];
-            
-        K = 2;
+	// your code goes here
+	
+	scanf("%I64d", &test);
+	
+	for (i = 2; i < N; i++)
+		t[i] = t[i - 1] + is(i); 
+	
+	while (test--) {
+		scanf("%I64d", &n);
+		
+		ans = 0;
+		for (i = 1; i <= n; i++) {
+			scanf("%I64d", &d[0][i]);
+			ans = max(ans, d[0][i]);
+		}
+		
+		K = 2;
         while ((1 << K) < n)
             K++;
-            
-        for (i = 1; i <= K; i++) {
-        //  printf("%d: ", i);
-            for (j = 1; j <= n - (1 << i) + 1; j++) {
-                d[i][j] = gcd(d[i - 1][j], d[i - 1][j + (1 << (i - 1))]);
-            }
-        }
-        
-        
-        for (i = 1; i < n; i++) {
-            int l = lower(1, i, i + 1, GCD(i, i + 1));
-            int r = upper(i, n, i, GCD(i, i + 1));
-            ans = max(ans, GCD(l, r) * (r - l + 1));
-        }
-        
-        printf("%I64d\n", ans);         
-    }
-
-    return 0;
+		
+		for (i = 1; i <= K; i++)
+			for (j = 1; j <= n - (1 << i) + 1; j++)
+				d[i][j] = gcd(d[i - 1][j], d[i - 1][j + (1 << (i - 1))]);
+		
+		for (i = 1; i < n; i++) {
+			int l = lower(1, i, i + 1, GCD(i, i + 1));
+			int r = upper(i, n, i, GCD(i, i + 1));
+			ans = max(ans, GCD(l, r) * (r - l + 1));
+		}
+		printf("%I64d\n", ans);
+	}
+	
+	
+	return 0;
 }
